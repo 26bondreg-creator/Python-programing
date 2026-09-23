3132d0a3890a23b3dbe17cf133980c03d862093c
26bondreg-creator <26bondreg@rbunagpur.in>
2026-09-23 16:09:33 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,9 @@
+seconds = int(input("Enter total seconds: "))
+
+hours = seconds // 3600
+minutes = (seconds % 3600) // 60
+seconds = seconds % 60
+
+print("Hours =", hours)
+print("Minutes =", minutes)
+print("Seconds =", seconds)
